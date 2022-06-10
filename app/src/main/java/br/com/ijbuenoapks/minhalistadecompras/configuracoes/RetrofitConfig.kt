bbdcb90de8ae7252d8a6d7b925c322cf44f2217a
Isaac José Bueno <ijbueno.apks@gmail.com>
2022-06-10 14:32:21 -0300

    Criado configuração para retrofit

---
+++ app/src/main/java/br/com/ijbuenoapks/minhalistadecompras/configuracoes/RetrofitConfig.kt
@@ -0,0 +1,24 @@
+package br.com.ijbuenoapks.minhalistadecompras.configuracoes
+
+import br.com.ijbuenoapks.minhalistadecompras.services.ProdutoService
+import retrofit2.Retrofit
+import retrofit2.converter.jackson.JacksonConverterFactory
+
+class RetrofitConfig {
+
+    private var retrofit : Retrofit? = null
+
+    fun RetrofitConfig(){
+        retrofit = Retrofit.Builder()
+            .baseUrl("http://localhost:8080/api/produtos/codigoBarras/")
+            .addConverterFactory(JacksonConverterFactory.create())
+            .build()
+    }
+
+    fun getProdutoService() : ProdutoService {
+        if(retrofit == null){
+            RetrofitConfig()
+        }
+        return retrofit!!.create(ProdutoService::class.java)
+    }
+}
