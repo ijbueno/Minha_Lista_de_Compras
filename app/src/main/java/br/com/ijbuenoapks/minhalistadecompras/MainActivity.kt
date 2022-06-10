b4e01435097bdc0be64bf99b0abf81871a3d0c9c
Isaac José Bueno <ijbueno.apks@gmail.com>
2022-06-10 10:40:00 -0300

    Criado variaveis e codigo para solicitação de permição de uso da camera e o acesso a ela

--- app/src/main/java/br/com/ijbuenoapks/minhalistadecompras/MainActivity.kt
+++ app/src/main/java/br/com/ijbuenoapks/minhalistadecompras/MainActivity.kt
@@ -1,11 +1,65 @@
 package br.com.ijbuenoapks.minhalistadecompras
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.budiyev.android.codescanner.CodeScanner
 
 class MainActivity : AppCompatActivity() {
+
+    //variaveis para requisição de permição e da camera
+    private val PERMISSION_REQUEST_CAMERA = 0
+    private lateinit var codeScanner: CodeScanner
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun requestCamera() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            codeScanner.startPreview()
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    Manifest.permission.CAMERA
+                )
+            ) {
+                ActivityCompat.requestPermissions(
+                    this@MainActivity,
+                    arrayOf(Manifest.permission.CAMERA),
+                    PERMISSION_REQUEST_CAMERA
+                )
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.CAMERA),
+                    PERMISSION_REQUEST_CAMERA
+                )
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST_CAMERA) {
+            if (grantResults.size == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                codeScanner.startPreview()
+            } else {
+                Toast.makeText(this, "Camera Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
